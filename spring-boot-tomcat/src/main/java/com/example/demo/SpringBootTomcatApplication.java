27fc673f0df468aa4b496be9a4ddd7253978bcbb
huruiyi <807776962@qq.com>
2023-03-16 05:58:21 +0800

    spring-boot-tomcat

---
+++ spring-boot-tomcat/src/main/java/com/example/demo/SpringBootTomcatApplication.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringBootTomcatApplication {
+
+    private static final Log logger = LogFactory.getLog(SpringBootTomcatApplication.class);
+
+    @Bean
+    protected ServletContextListener listener() {
+        return new ServletContextListener() {
+
+            @Override
+            public void contextInitialized(ServletContextEvent sce) {
+                logger.info("ServletContext initialized");
+            }
+
+            @Override
+            public void contextDestroyed(ServletContextEvent sce) {
+                logger.info("ServletContext destroyed");
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTomcatApplication.class, args);
+    }
+
+}
---
+++ spring-boot-tomcat/src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,41 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.nio.charset.Charset;
+import java.util.List;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+
+    //@Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return filter;
+    }
+
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        StringHttpMessageConverter converter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
+        return converter;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        converters.add(responseBodyConverter());
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false);
+    }
+}
