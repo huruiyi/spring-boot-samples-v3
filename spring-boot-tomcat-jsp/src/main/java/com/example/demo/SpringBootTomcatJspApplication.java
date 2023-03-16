63ecbffecc19f38c1fd9b8d47e52971d0cd04c5c
胡睿毅 <807776962@qq.com>
2023-03-16 09:49:43 +0800

    spring-boot-tomcat-jsp

--- spring-boot-tomcat-jsp/src/main/java/com/example/demo/SpringBootTomcatJspApplication.java
+++ spring-boot-tomcat-jsp/src/main/java/com/example/demo/SpringBootTomcatJspApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootTomcatJspApplication {
 
+	/**
+	 * 需运行于tomcat下，或者独立打开spring-boot-tomcat-jsp项目，直接运行
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTomcatJspApplication.class, args);
 	}
