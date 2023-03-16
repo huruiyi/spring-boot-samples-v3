115be6dc520c71a337d4919aa2d6eb9fd9e210a1
huruiyi <807776962@qq.com>
2023-03-16 19:18:35 +0800

    spring-boot-thymeleaf

--- spring-boot-thymeleaf/src/main/java/com/example/demo/SpringBootThymeleafApplication.java
+++ spring-boot-thymeleaf/src/main/java/com/example/demo/SpringBootThymeleafApplication.java
@@ -8,7 +8,6 @@ public class SpringBootThymeleafApplication {
 
 	/**
 	 * 需运行于tomcat下，或者独立打开spring-boot-tomcat-jsp项目，直接运行
-	 * @param args
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootThymeleafApplication.class, args);
