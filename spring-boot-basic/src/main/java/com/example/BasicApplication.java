32225c1e123f3e319cf609ecafe34fd0c36c34ff
胡睿毅 <807776962@qq.com>
2025-03-16 17:16:25 +0800

    bak

--- spring-boot-basic/src/main/java/com/example/BasicApplication.java
+++ spring-boot-basic/src/main/java/com/example/BasicApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BasicApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(BasicApplication.class, args);
+    try (var ctx = SpringApplication.run(BasicApplication.class, args)) {
+
+      System.out.println("# Beans: " + ctx.getBeanDefinitionCount());
+
+      var names = ctx.getBeanDefinitionNames();
+      Arrays.sort(names);
+      Arrays.asList(names).forEach(System.out::println);
+    }
   }
 
 }
